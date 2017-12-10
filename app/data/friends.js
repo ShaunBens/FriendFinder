c28b96316a38fc239eb281fee338b0d77b6754c7
Shaun Bens <bensshaun@gmail.com>
2017-12-10 02:15:24 +0000

    working out the bugs with ajax post

---
+++ app/data/friends.js
@@ -0,0 +1,35 @@
+var friends = [{
+    "name": "Steve Jobs",
+    "photo": "http://image.boomsbeat.com/data/images/full/209/jobs-jpg.jpg",
+    "scores": [5, 5, 5, 5, 5, 1, 2, 3, 1, 5]
+}, {
+    "name": "Elon Musk",
+    "photo": "https://i.ytimg.com/vi/tnBQmEqBCY0/maxresdefault.jpg",
+    "scores": [1, 5, 1, 4, 5, 2, 5, 1, 5, 1]
+}, {
+    "name": "Arnold Schwarzenegger",
+    "photo": "https://naqyr37xcg93tizq734pqsx1-wpengine.netdna-ssl.com/wp-content/uploads/2015/02/Arnold-Schwarzenegger-Champion-BodyBuilder.jpg",
+    "scores": [2, 4, 5, 2, 3, 4, 5, 1, 5, 2]
+}, {
+    "name": "Mark Zuckerberg",
+    "photo": "https://harvardgazette.files.wordpress.com/2017/03/mark-zuckerberg-headshot-11.jpg",
+    "scores": [5, 5, 1, 5, 5, 1, 5, 5, 1, 4]
+}, {
+    "name": "Kristen Stewart",
+    "photo": "https://typeset-beta.imgix.net/rehost%2F2016%2F9%2F13%2Fe799caa2-5184-410a-9412-a2fad7243134.jpg?w=740&h=493&fit=crop&crop=faces&auto=format&q=70",
+    "scores": [5, 1, 1, 1, 1, 5, 5, 5, 1, 1]
+}, {
+    "name": "Princess Leia",
+    "photo": "https://i.amz.mshcdn.com/rpLqLYbiaivUsgpka1yPlldpOlU=/950x534/filters:quality(90)/https%3A%2F%2Fblueprint-api-production.s3.amazonaws.com%2Fuploads%2Fcard%2Fimage%2F557489%2Fc7d21541-c0e6-4395-9fea-690a49a4de7a.jpg",
+    "scores": [1, 4, 5, 5, 3, 1, 1, 4, 5, 3]
+}, {
+    "name": "Miley Cyrus",
+    "photo": "http://thehollywoodunlocked.com/wp-content/uploads/2017/11/miley-cyrus-23.png",
+    "scores": [5, 2, 3, 5, 2, 1, 1, 3, 5, 1]
+}, {
+    "name": "Jennifer Lawrence",
+    "photo": "https: //hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/gettyimages-842866878-1512576734.jpg?crop=0.859xw:0.646xh;0.141xw,0.0145xh&resize=768:*",
+    "scores": [2, 4, 4, 3, 3, 4, 2, 1, 5, 5]
+}];
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -3,88 +3,24 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+var initHtml = require("./app/routing/htmlRoutes.js");
+var initApi = require("./app/routing/apiRoutes.js");
 
 // Sets up the Express App
 // =============================================================
 var app = express();
+
+initHtml(app);
+initApi(app);
 var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-// Star Wars Characters (DATA)
-// =============================================================
-var people = [{
-        routeName: "joe",
-        name: "Joe",
-        role: "Jedi Master",
-        age: 900,
-        forcePoints: 2000
-    },
-    {
-        routeName: "clarisa",
-        name: "Clarisa",
-        role: "Sith Lord",
-        age: 200,
-        forcePoints: 1200
-    },
-    {
-        routeName: "stacy",
-        name: "Stacy",
-        role: "Jedi Master",
-        age: 55,
-        forcePoints: 1350
-    }
-];
-
-// Routes
-// =============================================================
-
-// Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-});
-
-app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "survey.html"));
-});
-
-// Get all characters
-app.get("/all", function(req, res) {
-    res.json(characters);
-});
-
-// Search for Specific Character (or all characters) - provides JSON
-app.get("/api/:characters?", function(req, res) {
-    var chosen = req.params.characters;
-
-    if (chosen) {
-        console.log(chosen);
-
-        for (var i = 0; i < characters.length; i++) {
-            if (chosen === characters[i].routeName) {
-                return res.json(characters[i]);
-            }
-        }
-        return res.json(false);
-    }
-    return res.json(characters);
-});
-
-// Create New Characters - takes in JSON input
-app.post("/api/new", function(req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body-parser middleware
-    var newcharacter = req.body;
-    newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
-
-    console.log(newcharacter);
-
-    characters.push(newcharacter);
-
-    res.json(newcharacter);
-});
+app.use(express.static(path.join(__dirname, '/app/public')));
 
 // Starts the server to begin listening
 // =============================================================
